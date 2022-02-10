8413a9b69e65eeaf07b04338abd51b313c36ea95
Liam Strand <Liam.Strand@tufts.edu>
2022-02-10 14:40:00 -0500

    lecture notes

---
+++ notes/2/10.c
@@ -0,0 +1,17 @@
+/* 
+ * Locality
+ * 
+ * We can exploit this to make our programs faster
+ * 
+ * Spatial locality:
+ *      When blocks can be cached fewer times, and the important data
+ *      is in the cache.
+ *      The point is fewer trips back to main memory
+ * 
+ * Temporal locality:
+ *      When you access a piece of memory, it is likely that it will be 
+ *      accessed in the near future.
+ * 
+ * 
+ * 
+ */
