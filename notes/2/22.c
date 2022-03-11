e4b72887ce0ac7616473b2a1b30855faa1a0c174
Liam Strand <Liam.Strand@tufts.edu>
2022-03-10 21:46:13 -0500

    notes

---
+++ notes/2/22.c
@@ -0,0 +1,18 @@
+/* 
+ * 
+ * In addition to the normal operators (+, -, *, /), we also have the shift
+ * operators: (<<, >>).
+ * 
+ * << is the same thing as multiplying by 2
+ * >> is the same thing as dividing by 2
+ * 
+ * We also have the ~ (bitwise not), which flips all the bits
+ * 
+ * We also have the & (bitwise and), which takes only the 1s where there is a
+ *                                   1 in two inputs.
+ * We also have the |  (bitwise or), which takes all the 1s where there is a 1
+ *                                   in either of the inputs.
+ * 
+ * 
+ * 
+ */
